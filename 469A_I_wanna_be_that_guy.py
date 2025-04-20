6588c65f988a3779e1c1b3092657eb8883a0029d
Yuhesh Pandian P J <yuheshpandian@gmail.com>
2025-04-20 17:25:40 +0530

    Solved 469A

---
+++ 469A_I_wanna_be_that_guy.py
@@ -0,0 +1,15 @@
+total_levels: int = int(input())
+
+
+little_x_levels = input().split()[1::]
+little_y_levels = input().split()[1::]
+
+little_x_levels.extend(little_y_levels)
+
+levels_both_can_win = set(little_x_levels)
+for i in range(1, total_levels + 1):
+    if str(i) not in levels_both_can_win:
+        print("Oh, my keyboard!")
+        break
+else:
+    print("I become the guy.")
